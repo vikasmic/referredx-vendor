c231e6c943f955e3121b90c663ee67cbc4b42c85
vikasmic <vikasguptamic@gmail.com>
2023-12-23 16:09:13 +0530

    environment

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    name:"(PROD)",
+    apiBaseUrl: "https://be.referredx.com",
+}
---
+++ src/environment/environment.test.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    name:"(TEST)",
+    apiBaseUrl: "https://be.referredx.com",
+}
