ab1bc91391fcad804377ff573c0aa4fc8249087a
vikasmic <vikasguptamic@gmail.com>
2024-02-12 16:48:59 +0530

    toast added

---
+++ src/app/services/error.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorService {
+
+  constructor(private toastr: ToastrService) { }
+
+  showError(msg: string) {
+    this.toastr.error(msg);
+  }
+}
